72ab648be2c4272f33a7cefce858100f7c6b4614
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-05-07 12:19:26 +0300

    todo form add, sorted last 4 items in main page

--- js/components.js
+++ js/components.js
@@ -1,34 +1,46 @@
 Vue.component('note', {
-    props: ['title', 'todolist', 'editable'],
-    data: function () {
+   props: ['title', 'todolist', 'editable'],
+   data: function () {
       return {
-        key: this.$vnode.key
+         key: this.$vnode.key
       }
-    },
-    created: function() {
-        this.sortList();
-    },
-    methods: {
-        // Done items to end of the list
-        sortList: function() {
-            this.todolist.sort((a) => { return a.isDone == true? 1: -1 });
-        },
-        // Switch checkbox
-        check: function(i) {
-            this.todolist[i].isDone = !this.todolist[i].isDone;
-            this.sortList();
-        },
-        delete_note: function(i) {
-            let key = this.key;
-            this.$emit('show-popup', {
-                question: 'Удалить запись',
-                title: this.title,
-                note_id: this.key,
-                emit: {name:'delete-note', id: key}
-            })
-        }
-    },
-    template: `<div class="note">
+   },
+   created: function () {
+      this.sortList();
+   },
+   methods: {
+      // Move "done" items to end of the list
+      sortList: function () {
+         this.todolist.sort((a) => {
+            return a.isDone ? 1 : -1
+         });
+      },
+      // Switch checkbox
+      check: function (i) {
+         this.todolist[i].isDone = !this.todolist[i].isDone;
+         this.sortList();
+      },
+      // Removing note by index
+      delete_note: function (i) {
+         let key = this.key;
+         this.$emit('show-popup', {
+            question: 'Удалить запись',
+            title: this.title,
+            note_id: this.key,
+            emit: {
+               name: 'delete-note',
+               id: key
+            }
+         })
+      }
+   },
+   computed: {
+      // If editable == false > return last 4 else return full todo list
+      returnRightList: function() {
+        return this.editable ? this.todolist : this.todolist.slice(0, 4);
+      } 
+   },
+   template: `<div class="note">
                     <div class="note-head">
                         <div class="title-side"><h4>{{title}}</h4></div>
                         <div class="control-side">
@@ -47,7 +59,7 @@ Vue.component('note', {
                     <div class="note-body">
                         <ul class="todolist">
 
-                            <li v-for="(item, i) in todolist"
+                            <li v-for="(item, i) in returnRightList"
                                 :key="i" class="item"
                                 :class="{ checked: item.isDone}" >
 
@@ -62,51 +74,96 @@ Vue.component('note', {
                                         {{item.title}}
                                 </label>
                             </li>
+                           <li v-if="todolist.length > 4" class="item etc">...</li>
                         </ul>
                     </div>
                 </div>`
 });
 
 Vue.component('add-note-button', {
-    data: function() {
-        return {
-            isActive: false
-        }
-    },
-    methods: {
-
-    },
-    template: `<div class="note add-btn" :class="{active: isActive}">
+   data: function () {
+      return {
+         isActive: false,
+         newNoteTitle: '',
+         newItem: '',
+         newItems: []
+      }
+   },
+   updated: function(){
+      
+       
+   },
+   methods: {
+      checkItem: function() {
+         if (this.newItem != '') {
+            this.newItems.push({
+               title: this.newItem,
+               isDone: false
+            });
+            this.newItem = '';
+         }
+      },
+      returnItem: function() {
+         if (this.newItem == '' && this.newItems.length > 0) {
+            // Cut lust item from todolist
+            // and paste to current input then focus 
+            let lastItem = this.newItems.splice(this.newItems.length-1, 1);
+            this.newItem = lastItem[0].title;
+            setTimeout(function() {
+               document.getElementById('add-item-input').focus()
+            }, 100);
+         }
+      }
+   },
+   template: `<div class="note add-btn" :class="{active: isActive}">
                     <div v-if="isActive" class="form-wrapp">
                         <h3>Добавление заметки</h3>
-                        <input type="text" class="form-input title" placeholder="Заголовок">
+                        <input type="text" v-model="newNoteTitle" class="form-input title" placeholder="Заголовок">
                         <ul class="todolist">
-                            <li>
-                                <input type="checkbox" disabled><input type="text" class="form-input item" placeholder="+ Добавить пункт">
-                            </li>
-                        </ul>
+                           <li class="item" v-for="(item, i) in newItems"><label 
+                                    :for="'todo-new'+i">
+                                    <input
+                                        type="checkbox"
+                                        :id="'todo-new'+i"
+                                        :checked="item.isDone">
+                                        {{item.title}}
+                                </label>
+                           </li>
 
+                            <li class="item" >
+                                <input type="checkbox" disabled><input id="add-item-input" v-on:keyup.enter="checkItem" v-on:keyup.delete.prevent="returnItem" v-model="newItem" type="text" class="form-input item" placeholder="+ Добавить пункт">
+                            </li>
+
+                           <li v-if="newItem != ''" class="item">
+                                <input type="checkbox" disabled><input v-on:keyup="checkItem"  type="text" class="form-input item" placeholder="+ Добавить пункт">
+                            </li>
+
+                        </ul>
+                        <div class="note-footer">
+                            <button class="btn cancel">Отмена</button>
+                            <button class="btn confirm"><i class="fa fa-plus" aria-hidden="true"></i> Добавить</button>
+                        </div>
                     </div>
-                    <div v-else v-on:click="isActive = true">
+                    <div v-else v-on:click.prevent="isActive = true">
                         <span class="add-note"><i class="fa fa-plus" aria-hidden="true"></i> Добавить заметку</span>
                     </div>
                 </div>`
 })
 
 Vue.component('popup', {
-    props: ['dataPopup'],
-    data: function() {
-        return {}
-    },
-    methods: {
-        confirmed(data) {
-            this.$emit('done-popup', data)
-        },
-        close() {
-            this.$emit('close-popup')
-        }
-    },
-    template: `<div class="popup-wrapp">
+   props: ['dataPopup'],
+   data: function () {
+      return {}
+   },
+   methods: {
+      confirmed(data) {
+         this.$emit('done-popup', data)
+      },
+      close() {
+         this.$emit('close-popup')
+      }
+   },
+   template: `<div class="popup-wrapp">
                     <transition name="fade">
                     <div class="popup">
                         <div class="note-head">
--- js/script.js
+++ js/script.js
@@ -1,75 +1,83 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        pages: {
-            home: 'main-page',
-            note: 'note-page'
-        },
-        currpage: false,
-        popup: false,
-        notes: [
-            {
-                title: 'Тестовая запись 1',
-                todo_items: [
-                    {
-                        title: 'Сделать Первое',
-                        isDone: false
+   el: '#app',
+   data: {
+      pages: {
+         home: 'main-page',
+         note: 'note-page'
+      },
+      currpage: false,
+      popup: false,
+      notes: [
+         {
+            title: 'Тестовая запись 1',
+            todo_items: [
+               {
+                  title: 'Сделать Первое',
+                  isDone: false
                     },
-                    {
-                        title: 'Сделать Второе',
-                        isDone: false
+               {
+                  title: 'Сделать Второе',
+                  isDone: false
                     },
                 ]
             },
-            {
-                title: 'Еще одна запись',
-                todo_items: []
+         {
+            title: 'Еще одна запись',
+            todo_items: []
             },
-            {
-                title: 'Тестовая запись 2',
-                todo_items: [
-                    {
-                        title: 'Сделать АБВ',
-                        isDone: false
+         {
+            title: 'Тестовая запись 2',
+            todo_items: [
+               {
+                  title: 'Сделать АБВ',
+                  isDone: false
                     },
-                    {
-                        title: 'Сделать БГД',
-                        isDone: true
+               {
+                  title: 'Сделать БГД',
+                  isDone: true
                     },
-                    {
-                        title: 'Сделать ЕЁЖ',
-                        isDone: false
+               {
+                  title: 'Сделать ЕЁЖ',
+                  isDone: false
                     },
-                    {
-                        title: 'Сделать ЗИКл',
-                        isDone: true
+               {
+                  title: 'Сделать ЗИКл',
+                  isDone: true
+                    },
+               {
+                  title: 'Сделать ЗИКл',
+                  isDone: true
+                    },
+               {
+                  title: 'Сделать ЗИКл',
+                  isDone: true
                     },
                 ]
             }
         ]
-    },
-    created: function() {
-        this.currpage = this.pages.home
-    },
-    methods: {
-        showPopup(data) {
-            this.popup = data
-        },
-        // Popup handler
-        donePopup(data) {
-            if (data.name == 'delete-note') this.deleteNote(data.id);
-        },
-        // Remove 'note' from array with shift 
-        deleteNote(id){
-            this.notes.splice(id, 1);
-            this.clearPopup();
-        },
-        // Clear popup data and close popup block
-        clearPopup() {
-            this.popup = false
-        },
-        pushNote(data){
+   },
+   created: function () {
+      this.currpage = this.pages.home
+   },
+   methods: {
+      showPopup(data) {
+         this.popup = data
+      },
+      // Popup handler
+      donePopup(data) {
+         if (data.name == 'delete-note') this.deleteNote(data.id);
+      },
+      // Remove 'note' from array with shift 
+      deleteNote(id) {
+         this.notes.splice(id, 1);
+         this.clearPopup();
+      },
+      // Clear popup data and close popup block
+      clearPopup() {
+         this.popup = false
+      },
+      pushNote(data) {
 
-        }
-    }
+      }
+   }
 });
